a4fcafc2b53247b11a257d15aafd2c6fc135c7bb
Skixrr <camrongeorgeshort@gmail.com>
2024-11-08 14:31:39 +0000

    Update linkedList.py

--- DataStructures/linkedList.py
+++ DataStructures/linkedList.py
@@ -6,22 +6,36 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = Node('empty')
+        self.tail = None
+        self.hasHead = False
     
     def add(self,new_data): #Add to start
-        new_node = Node(new_data) #Create head node
-        new_node.next = self.head #Link to old head
-        self.head = new_node #Make into new head
-    
+        if self.hasHead == False:
+            new_node = Node(new_data) #Create head node
+            new_node.next = self.head #Link to old head
+            self.head = new_node #Make into new head
+            self.tail = new_node
+            self.hasHead = True
+        else:
+            new_node = Node(new_data)
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
     def delStart(self):
         if self.head.next != None:
             self.head = self.head.next
         else:
             self.head = None
-
+    
     def printList(self):
-        while self.head.next != None: # Go over list
-            print(new_list.head.data)
-            self.delStart()
+        self.currNode = self.head
+        def nextNode(self):
+            self.currNode = self.currNode.next
+        while self.currNode.next != None:
+            print(self.currNode.data)
+            nextNode(self)
+        print(self.tail.data)
+
 
 new_list = LinkedList()
 new_list.add(69)
