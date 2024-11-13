2a9f2da3deba61bb2f160a8c77ff8b03a58c3ffd
Skixrr <camrongeorgeshort@gmail.com>
2024-11-13 09:21:08 +0000

    Update tree.py

--- DataStructures/tree.py
+++ DataStructures/tree.py
@@ -1,8 +1,11 @@
+import ctypes
+
 class Node:
-    def __init__(self,data:int,depth: int,next=[]):
+    def __init__(self,data:int,depth: int):
         self.data = data
-        self.next = next
+        self.next = []
         self.depth = depth
+        self.location = id(self)
 
 class Tree:
     def __init__(self):
@@ -12,10 +15,12 @@ class Tree:
     def add(self,data,root=None):
         if root == None:
             root = self.master
+        else:
+            root = ctypes.cast()
         if self.master == None:
             self.master = Node(data,1)
         else:
-            root.next.append(Node(data,root.depth + 1,[]))
+            root.next.append(Node(data,root.depth + 1))
     
     def output(self):
         def recurse(currNode):
@@ -30,3 +35,5 @@ new_tree.add(4)
 new_tree.add(5)
 new_tree.add(6)
 new_tree.output()
+
+#how do i add to an existing node apart from master...
